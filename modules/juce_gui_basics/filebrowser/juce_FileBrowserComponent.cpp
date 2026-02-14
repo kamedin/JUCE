3a66cfee688d2be52b274358a95f00d6c21ab62e
Laureano López <iekamede@gmail.com>
2026-02-14 12:39:59 -0300

    Fix FileBrowserComponent
    - don't clear rootPaths in getDefaultRoots()
    - allow multiple pathbox separators

--- modules/juce_gui_basics/filebrowser/juce_FileBrowserComponent.cpp
+++ modules/juce_gui_basics/filebrowser/juce_FileBrowserComponent.cpp
@@ -259,7 +259,8 @@ void FileBrowserComponent::setRoot (const File& newRootDirectory)
             }
 
             if (! alreadyListed)
-                currentPathBox.addItem (path, currentPathBox.getNumItems() + 2);
+                currentPathBox.addItem (path, currentPathBox.getNumItems() + 1
+                                        + (int) std::count (rootNames.begin(), rootNames.end(), String()));
         }
     }
 
@@ -534,7 +535,6 @@ void FileBrowserComponent::getDefaultRoots (StringArray& rootNames, StringArray&
    #if JUCE_WINDOWS
     Array<File> roots;
     File::findFileSystemRoots (roots);
-    rootPaths.clear();
 
     for (int i = 0; i < roots.size(); ++i)
     {
